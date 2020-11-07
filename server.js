ca870499f8686b722881d2e461d006ffd30bfdd0
SURYA MAHLA <mahlasurya@gmail.com>
2020-11-07 20:13:49 +0530

    new changes

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bannerSchema = require("./model")
+const express = require("express");
+const app = express();
 
-const app = express()
+const port = process.env.PORT || 3456;
 
-const port = process.env.PORT || 8080
+app.get("/", (req, res) => {
+  res.send("Hello.");
+});
 
-mongoose.connect("mongodb+srv://hunter:hunt_vk@cluster0-x3aca.mongodb.net/IRS_ECOMMERCE?retryWrites=true&w=majority", {
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}, () => console.log("mongodb connected"))
-
-app.get("/", (req,res) => {
-    bannerSchema.find({}).then(docs => {
-        res.json(docs)
-    })
-})
-
-app.listen(port, () => console.log(`app listening at port ${port}`))
+app.listen(port, () => console.log(`app listening at port ${port}`));
